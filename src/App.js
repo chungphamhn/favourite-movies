740ba89996381e7625a6ce6583f2d1cae0892f43
chungphamhn <chungphamhn@gmail.com>
2021-11-09 15:43:33 +0000

    project done

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ class App extends Component {
       <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
+          <h1 className="App-title">ReactND - Udacity - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
 		
